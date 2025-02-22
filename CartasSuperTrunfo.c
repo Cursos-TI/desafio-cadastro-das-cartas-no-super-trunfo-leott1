c53cea5158807c55b4c5d842114359b8dd575b0a
leott1 <leonardotiago.771@gmail.com>
2025-02-22 00:51:01 +0000

    adicionado estado, medias, nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,41 +1,48 @@
 #include <stdio.h>
 
 int main() {
-    // definindo variaveis 
+    // definindo variaveis intuitivas
     int qtdpon;
-    char nomecit[50], codcit[50];
-    double  pop, area, pib;
+    char estado[50], nomecit[50], codcit[50];
+    float  pop, area, pib, densp, pbp;
+    
+    // solicitando entradas do usuario
+    printf("Digite o estado: \n");
+    scanf("%s", &estado);
 
-    // solicitando entradas do usuario, separados por tipo de variavel (char)
     printf("Digite o código da cidade: \n");
     scanf("%s", &codcit);
 
     printf("Digite o nome: \n");
     scanf("%s", &nomecit);
 
-    // solicitando entradas do usuario, separados por tipo de variavel (double)
     printf("Digite a população: \n");
-    scanf("%lf", &pop);
+    scanf("%f", &pop);
 
     printf("Digite a área: \n");
-    scanf("%lf", &area);
+    scanf("%f", &area);
 
     printf("Digite o PIB: \n");
-    scanf("%lf", &pib);
+    scanf("%f", &pib);
 
-    // solicitando entradas do usuario, separados por tipo int
     printf("Digite a quantidade de pontos turísticos: \n");
     scanf("%d", &qtdpon);
 
-    // exibindo entradas ao usuário separados por tipo char
-    printf("O codigo de sua cidade é: %s\nO nome é: %s\n", codcit, nomecit);
+    // realizando operacoes aritmeticas e armazenando nas variaveis para exibir resultado de densidade populacional e pib per capita
+    densp = (float) (pop / area);
+    pbp = (float) (pib / pop);
+
+    // exbindo resultados
+
+    printf("O estado é: %s\nO codigo de sua cidade é: %s\nO nome é: %s\n", estado, codcit, nomecit);
   
-
-    // exibindo entradas ao usuário separados por tipo double
-    printf("A população é:  %lf\nA área é: %lf\nO PIB é: %lf\n", pop, area, pib);
-
-    // exibindo entradas ao usuário separados por tipo int
+    printf("A população é:  %.2f\nA área é: %.2f\nO PIB é: %.2f\n", pop, area, pib);
+   
     printf("A quantidade de pontos turísticos são: %d\n", qtdpon);
+
+    printf("A densidade populacional é: %.2f\n", densp);
+    
+    printf("O pib percapita é: %.2f\n", pbp);
   
 
 
